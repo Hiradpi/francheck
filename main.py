d1183d7c56836e3ba8042aa8410fed5b41def927
Hiradpi <78161225+Hiradpi@users.noreply.github.com>
2024-05-20 17:38:17 +0330

    Update main.py
    added --mode

--- main.py
+++ main.py
@@ -8,36 +8,91 @@ parser = argparse.ArgumentParser(description='Francheck a tool for checking fran
 
 parser.add_argument('--code', help='the code on the buy link at the pid value get request https://my.frantech.ca/cart.php?a=add&pid=1411 here is 1411')
 parser.add_argument('--delay', help='How much delay you want to check for avalability in secconds (recomended Is 300)')
+parser.add_argument('--mode', help="Mode 0: sends data only if it's avalable , Mode 1: sneds data no matter what , Mode 2: sends data only if its not avalable")
 
 args = parser.parse_args()
 
 
+# replace this part with your webhook URL 
+webhook_url = 'https://discord.com/api/webhooks/...'
 
 
-def checkav(code , delay):
+def checkav(code , delay , mode):
     if delay == None or delay == "":
         delay = 3
     else:
         delay = int(delay)
+    if mode == None or mode == "":
+        mode = 0
+    else:
+        mode = int(mode)
 
     while True:
         Request = requests.get(f"https://buyvm.hasstock.net/api/package/{code}.json")
         data = Request.json()
         print(data)
-        webhook_url = 'https://discord.com/api/webhooks/1126068796038316053/SwjllZjWm839D395pXRsvYSFKtQGZwu1gbfRicnM-qFggY_JhaoBMo_8BnEUDmnOcb3D'
-
         message = f"Data: {data['name']} (ID: {data['id']})\nQuantity: {data['quantity']}\nAvailable: {data['hasStock']}\nLink: {data['link']}"
-
         payload = {
-            'content': message
+            "embeds": [
+                {
+                    "title": "Product Availability",
+                    "description": "Here's the availability status of the product:",
+                    "color": 16761867,
+                    "fields": [
+                        {
+                            "name": "Name",
+                            "value": data['name'],
+                            "inline": True
+                        },
+                        {
+                            "name": "ID",
+                            "value": data['id'],
+                            "inline": True
+                        },
+                        {
+                            "name": "Quantity",
+                            "value": data['quantity'],
+                            "inline": True
+                        },
+                        {
+                            "name": "Available",
+                            "value": data['hasStock'],
+                            "inline": True
+                        }
+                    ],
+                    "footer": {
+                        "text": "For more information, visit the product page:",
+                        "icon_url": "https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png"
+                    },
+                    "url": data['link']
+                }
+            ]
         }
 
-        response = requests.post(webhook_url, json=payload)
+        if mode == 0:
+            if data["hasStock"] != False and data["quantity"] != 0:
+                response = requests.post(webhook_url, json=payload)
+                if response.status_code == 204:
+                    print('Data sent to Discord successfully.')
+                else:
+                    print(f'Failed to send data to Discord. Status code: {response.status_code}')
+        if mode == 1:
+            response = requests.post(webhook_url, json=payload)
+            if response.status_code == 204:
+                print('Data sent to Discord successfully.')
+            else:
+                print(f'Failed to send data to Discord. Status code: {response.status_code}')
+        if mode == 2:
+            if data["hasStock"] == False and data["quantity"] == 0:
+                response = requests.post(webhook_url, json=payload)
+                if response.status_code == 204:
+                    print('Data sent to Discord successfully.')
+                else:
+                    print(f'Failed to send data to Discord. Status code: {response.status_code}')
 
-        if response.status_code == 204:
-            print('Data sent to Discord successfully.')
-        else:
-            print(f'Failed to send data to Discord. Status code: {response.status_code}')
+            
+
+
         time.sleep(delay)
 
 
@@ -90,7 +145,7 @@ def select_and_print(category_list, category_name):
         if 1 <= selected_number <= len(category_list):
             selected_item = category_list[selected_number - 1][1]
             print("\nChecking avalability of",selected_item[0],"every 10 secconds")
-            checkav(selected_item[1],args.delay)
+            checkav(selected_item[1],args.delay,args.mode)
         else:
             print("Invalid number. Please enter a valid number.")
     except ValueError:
@@ -103,6 +158,6 @@ def select_and_print(category_list, category_name):
 if args.code == None:
     noarg()
 else:
-    checkav(args.code , args.delay)
+    checkav(args.code , args.delay , args.mode)
 
 
